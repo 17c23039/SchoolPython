ace06e3926ab2e4d3dc6afa72d63426f70748760
Liam Dixon <17c23039@marling.school>
2020-09-08 11:14:42 +0000

    First lines of calc. L1.

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-age = input("Heyo, how old are you?")
-if
+number1 = float(input("Ima do some quick maths, gimme a number. "))
+sumtype = input("K, now give me an operator please. Like, add or smt. I can only rly do + - * and / bc im pretty simple. ")
